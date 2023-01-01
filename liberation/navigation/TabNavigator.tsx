b5f124323ef0424550f3532a533ad098e5387d84
functionalfunction <giraysoyturk@gmail.com>
2023-01-01 22:59:54 +0300

    new tab icon

--- liberation/navigation/TabNavigator.tsx
+++ liberation/navigation/TabNavigator.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {createBottomTabNavigator, } from '@react-navigation/bottom-tabs';
 import BookListScreen from '../screens/BookList';
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
-import {faIgloo} from '@fortawesome/free-solid-svg-icons';
+import {faBook} from '@fortawesome/free-solid-svg-icons';
 
 export type TabBarParamList = {
     BookListScreen: undefined;
@@ -34,8 +34,8 @@ const TabNavigator = () => {
                 name="BookListScreen"
                 component={BookListScreen}
                 options={{
-                    tabBarActiveTintColor: 'yellow',
-                    tabBarIcon: ({ color, size }) => <FontAwesomeIcon icon={faIgloo} color={color} size={size} />,
+                    tabBarActiveTintColor: 'blue',
+                    tabBarIcon: ({ color, size }) => <FontAwesomeIcon icon={faBook} color={color} size={size} />,
                 }}
             />
         </Tab.Navigator>
